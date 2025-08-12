1e77d2202c452975fe90eb45b29dbe8f4e276cd6
Nandan Lalani <80481008+nandanlalani@users.noreply.github.com>
2025-08-12 13:12:50 +0530

    Add files via upload

---
+++ studentModel.dart
@@ -0,0 +1,43 @@
+class Student {
+  int? studentId;
+  String? studentName;
+  String? studentEnrollmentNo;
+  String? semester;
+  String? branch;
+  String? mobile;
+  String? password;
+
+  Student({
+    this.studentId,
+    this.studentName,
+    this.studentEnrollmentNo,
+    this.semester,
+    this.branch,
+    this.mobile,
+    this.password,
+  });
+
+  Map<String, dynamic> toMap() {
+    return {
+      'studentId': studentId,
+      'studentName': studentName,
+      'studentEnrollmentNo': studentEnrollmentNo,
+      'semester': semester,
+      'branch': branch,
+      'mobile': mobile,
+      'password': password,
+    };
+  }
+
+  factory Student.fromMap(Map<String, dynamic> map) {
+    return Student(
+      studentId: map['studentId'],
+      studentName: map['studentName'],
+      studentEnrollmentNo: map['studentEnrollmentNo'],
+      semester: map['semester'],
+      branch: map['branch'],
+      mobile: map['mobile'],
+      password: map['password'],
+    );
+  }
+}
